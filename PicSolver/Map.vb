0732a4862eee931649ad8bb489cf5187a3d21963
MkbMain <michael.bourke@live.co.uk>
2021-12-27 08:09:26 +0000

    Refactor

---
+++ PicSolver/Map.vb
@@ -0,0 +1,158 @@
+Imports System.Drawing
+
+Class Map
+    Private Property Dots As Dot()()
+    Private Property Solved As Boolean
+    Sub New(ByVal imagePath As String)
+        LoadFromIamge(imagePath)
+    End Sub
+
+    Public ReadOnly Property Size As Size
+        Get
+            Return New Size(Dots.Length, Dots.First().Length)
+        End Get
+    End Property
+
+    Public Function GetDot(ByVal point As Point) As Dot
+        Return GetDot(point.X, point.Y)
+    End Function
+    Public Function GetDot(ByVal x As Integer, ByVal y As Integer) As Dot
+        Return Dots(x)(y)
+    End Function
+
+    Private Sub LoadFromIamge(ByVal imagePath As String)
+        Using image = New Bitmap(imagePath)
+            Dots = Enumerable.Range(0, image.Size.Width).Select(Function(t) New Dot(image.Size.Height - 1) {}).ToArray()
+            Dim func As Action(Of Integer, Integer) = Function(x, y)
+                                                          If image.GetPixel(x, y).R = 0 And image.GetPixel(x, y).G = 0 And image.GetPixel(x, y).B = 0 Then
+                                                              Dots(x)(y) = New Dot(True, New Point(x, y))
+                                                          ElseIf image.GetPixel(x, y).G > 0 And image.GetPixel(x, y).R < 255 And image.GetPixel(x, y).G <= 255 Then
+                                                              Dots(x)(y) = New Dot(False, New Point(x, y), True)
+                                                          ElseIf image.GetPixel(x, y).G < 255 And image.GetPixel(x, y).R > 0 And image.GetPixel(x, y).R <= 255 Then
+                                                              Dots(x)(y) = New Dot(False, New Point(x, y), False, True)
+                                                          Else
+                                                              Dots(x)(y) = New Dot(False, New Point(x, y))
+                                                          End If
+                                                      End Function
+            IterateThroughMap(func)
+        End Using
+    End Sub
+
+    Public Sub SaveSoultion(ByVal SavePath As String)
+        If Solved = False Then
+            Solve()
+        End If
+
+        Using image As New Bitmap(Size.Width, Size.Height)
+            Dim graphics As Graphics = System.Drawing.Graphics.FromImage(image)
+            Dim blackPen As Pen = New Pen(Brushes.Black)
+            Dim whitePen As Pen = New Pen(Brushes.White)
+            Dim redPen As Pen = New Pen(Brushes.Red)
+
+            For x As Integer = 0 To Me.Size.Width - 1
+                For y As Integer = 0 To Me.Size.Height - 1
+                    Dim dot As Dot = Me.GetDot(x, y)
+                    Dim point As Point = New Point(x, y)
+                    Dim rectangle = New Rectangle(point, New Size(1, 1))
+
+                    If dot.Wall Then
+                        graphics.FillRectangle(Brushes.Black, rectangle)
+                        graphics.DrawRectangle(blackPen, rectangle)
+                    ElseIf dot.PathUsed Or dot.EndPoint Then
+                        graphics.FillRectangle(Brushes.Red, rectangle)
+                        graphics.DrawRectangle(redPen, rectangle)
+                    Else
+                        graphics.FillRectangle(Brushes.White, rectangle)
+                        graphics.DrawRectangle(whitePen, rectangle)
+                    End If
+                Next
+            Next
+            image.Save(SavePath, System.Drawing.Imaging.ImageFormat.Png)
+        End Using
+    End Sub
+
+    Public Sub Solve()
+        If Solved Then
+            Return
+        End If
+        Dim positions = Me.GetStartAndEndPoint()
+        Dim currentLoc = positions.startPos
+        Dim endReached = False
+        While (endReached = False)
+            Dim dot = Me.GetDot(currentLoc)
+            dot.PathUsed = True
+            Dim allOptions = Me.GetAroundDot(currentLoc)
+            Dim forward = allOptions.Where(Function(e) Me.GetDot(e).Wall = False And Me.GetDot(e).PathUsed = False And Me.GetDot(e).DeadEnd = False).ToArray()
+            If (forward.Any() = False) Then
+                ' backTrackTime
+                If dot.StartPoint Then
+                    ' we are screwed
+                    Throw New Exception("Ok we are F%!^&d")
+                End If
+                currentLoc = dot.PreviousLocation
+                dot.PathUsed = False
+                dot.DeadEnd = True
+                Continue While
+            End If
+
+            Dim nextDot = Me.GetDot(forward.First())
+            nextDot.PreviousLocation = dot.Location
+            If nextDot.EndPoint Then
+                endReached = True
+                Solved = True
+            Else
+
+                currentLoc = nextDot.Location
+            End If
+        End While
+    End Sub
+
+    Public Function GetAroundDot(ByRef pos As Point) As List(Of Point)
+        Dim maxX = Dots.Length
+        Dim maxY = Dots.First().Length
+        Dim moves = New List(Of Point)() From {New Point(pos.X - 1, pos.Y), New Point(pos.X + 1, pos.Y), New Point(pos.X, pos.Y - 1), New Point(pos.X, pos.Y + 1)}
+
+        Return moves _
+            .Where(Function(e) e.X < maxX And e.X > -1) _
+            .Where(Function(e) e.Y > -1 And e.Y < maxY).ToList()
+    End Function
+
+    Public Function GetStartAndEndPoint() As (startPos As Point, endPos As Point)
+        Dim start As Point = Nothing
+        Dim ends As Point = Nothing
+        Dim func As Action(Of Integer, Integer) = Function(x, y)
+            If Dots(x)(y).StartPoint Then
+                If start <> Nothing Then
+                    Throw New Exception("Can't have multiple starts")
+                End If
+                start = New Point(x, y)
+            End If
+            If Dots(x)(y).EndPoint Then
+                If ends <> Nothing Then
+                    Throw New Exception("Can't have multiple ends")
+                End If
+                ends = New Point(x, y)
+            End If
+        End Function
+
+        IterateThroughMap(func)
+
+        If start = Nothing Or ends = Nothing Then
+            Throw New Exception("no start or end found")
+        End If
+
+        Return (start, ends)
+    End Function
+
+
+
+
+
+    Public Sub IterateThroughMap(ByRef func As Action(Of Integer, Integer))
+        For x As Integer = 0 To Dots.Length - 1
+            For y As Integer = 0 To Dots.First().Length - 1
+                func(x, y)
+            Next
+        Next
+    End Sub
+End Class
--- PicSolver/Program.vb
+++ PicSolver/Program.vb
@@ -1,5 +1,4 @@
 Imports System
-Imports System.Drawing
 
 
 Module Program
@@ -10,41 +9,12 @@ Module Program
         If args.Length < 1 Then
             Console.WriteLine("Picture needs to be a argument")
         End If
-        Dim map = GetMap(String.Join(" ", args))
-        Dim positions = GetStartAndEndPoint(map)
+        Dim map = New Map(String.Join(" ", args))
+        map.SaveSoultion("D:\ourPicSol.png")
 
-        Dim currentLoc = positions.startPos
-        Dim endReached = False
-        While (endReached = False)
-            Dim dot = map(currentLoc.X)(currentLoc.Y)
-            dot.PathUsed = True
-            Dim allOptions = GetAroundDot(map, currentLoc)
-            Dim forward = allOptions.Where(Function(e) map(e.X)(e.Y).Wall = False And map(e.X)(e.Y).PathUsed = False And map(e.X)(e.Y).DeadEnd = False).ToArray()
-            If (forward.Any() = False) Then
-                ' backTrackTime
-                If dot.StartPoint Then
-                    ' we are screwed
-                    Throw New Exception("Ok we are F%!^&d")
-                End If
-                currentLoc = dot.PreviousLocation
-                dot.PathUsed = False
-                dot.DeadEnd = True
-                Continue While
-            End If
-
-            Dim nextDot = map(forward.First().X)(forward.First().Y)
-            nextDot.PreviousLocation = dot.Location
-            If nextDot.EndPoint Then
-                endReached = True
-            Else
-
-                currentLoc = nextDot.Location
-            End If
-        End While
-
-        For x As Integer = 0 To map.Length - 1
-            For y As Integer = 0 To map.First().Length - 1
-                Dim dot As Dot = map(x)(y)
+        For x As Integer = 0 To map.Size.Width - 1
+            For y As Integer = 0 To map.Size.Height - 1
+                Dim dot As Dot = map.GetDot(x, y)
                 Console.SetCursorPosition(x + 1, y + 1)
 
                 If dot.Wall Then
@@ -62,72 +32,4 @@ Module Program
     End Sub
 
 
-
-
-    Public Function GetAroundDot(ByRef map As Dot()(), ByRef pos As Point) As List(Of Point)
-        Dim maxX = map.Length
-        Dim maxY = map.First().Length
-        Dim moves = New List(Of Point)() From {New Point(pos.X - 1, pos.Y), New Point(pos.X + 1, pos.Y), New Point(pos.X, pos.Y - 1), New Point(pos.X, pos.Y + 1)}
-
-        Return moves _
-        .Where(Function(e) e.X < maxX And e.X > -1) _
-            .Where(Function(e) e.Y > -1 And e.Y < maxY).ToList()
-    End Function
-
-    Public Function GetStartAndEndPoint(ByRef map As Dot()()) As (startPos As Point, endPos As Point)
-        Dim start As Point = Nothing
-        Dim ends As Point = Nothing
-        Dim func As Action(Of Integer, Integer, Dot()()) = Function(x, y, miniMap)
-                                                               If miniMap(x)(y).StartPoint Then
-                                                                   If start <> Nothing Then
-                                                                       Throw New Exception("Can't have multiple starts")
-                                                                   End If
-                                                                   start = New Point(x, y)
-                                                               End If
-                                                               If miniMap(x)(y).EndPoint Then
-                                                                   If ends <> Nothing Then
-                                                                       Throw New Exception("Can't have multiple ends")
-                                                                   End If
-                                                                   ends = New Point(x, y)
-                                                               End If
-                                                           End Function
-
-        IterateThroughMap(map, func)
-
-        If start = Nothing Or ends = Nothing Then
-            Throw New Exception("no start or end found")
-        End If
-
-        Return (start, ends)
-    End Function
-
-    Public Function GetMap(ByVal path As String) As Dot()()
-        Using image = New Bitmap(path)
-            Dim minimap2 = Enumerable.Range(0, image.Size.Width).Select(Function(t) New Dot(image.Size.Height - 1) {}).ToArray()
-            Dim func As Action(Of Integer, Integer, Dot()()) = Function(x, y, minimap)
-                                                                   If image.GetPixel(x, y).R = 0 And image.GetPixel(x, y).G = 0 And image.GetPixel(x, y).B = 0 Then
-                                                                       minimap(x)(y) = New Dot(True, New Point(x, y))
-                                                                   ElseIf image.GetPixel(x, y).G > 0 And image.GetPixel(x, y).R < 255 And image.GetPixel(x, y).G <= 255 Then
-                                                                       minimap(x)(y) = New Dot(False, New Point(x, y), True)
-                                                                   ElseIf image.GetPixel(x, y).G < 255 And image.GetPixel(x, y).R > 0 And image.GetPixel(x, y).R <= 255 Then
-                                                                       minimap(x)(y) = New Dot(False, New Point(x, y), False, True)
-                                                                   Else
-                                                                       minimap(x)(y) = New Dot(False, New Point(x, y))
-                                                                   End If
-                                                               End Function
-            IterateThroughMap(minimap2, func)
-
-            Return minimap2
-        End Using
-    End Function
-
-
-
-    Public Sub IterateThroughMap(ByRef map As Dot()(), ByRef func As Action(Of Integer, Integer, Dot()()))
-        For x As Integer = 0 To map.Length - 1
-            For y As Integer = 0 To map.First().Length - 1
-                func(x, y, map)
-            Next
-        Next
-    End Sub
 End Module
