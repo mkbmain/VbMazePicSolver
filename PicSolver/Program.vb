d33627f215ee831b59d2d5c7c368a899562be4ef
MkbMain <michael.bourke@live.co.uk>
2021-12-27 08:38:43 +0000

    q

--- PicSolver/Program.vb
+++ PicSolver/Program.vb
@@ -4,7 +4,7 @@ Imports System
 Module Program
     Sub Main(args As String())
 #If DEBUG Then
-        args = {"D:\ourPic.png"}
+        args = {"D:\bigPic.png"}
 #End If
         If args.Length < 1 Then
             Console.WriteLine("Picture needs to be a argument")
@@ -13,21 +13,21 @@ Module Program
         Dim map = New Map(path)
         map.SaveSoultion(String.Join("\", path.Split("\").Take(path.Split("\").Length - 1)) + "\" + "solution.png")
 
-        For x As Integer = 0 To map.Size.Width - 1
-            For y As Integer = 0 To map.Size.Height - 1
-                Dim dot As Dot = map.GetDot(x, y)
-                Console.SetCursorPosition(x + 1, y + 1)
+        'For x As Integer = 0 To map.Size.Width - 1
+        '    For y As Integer = 0 To map.Size.Height - 1
+        '        Dim dot As Dot = map.GetDot(x, y)
+        '        Console.SetCursorPosition(x + 1, y + 1)
 
-                If dot.Wall Then
-                    Console.BackgroundColor = ConsoleColor.White
-                ElseIf dot.PathUsed Or dot.EndPoint Then
-                    Console.BackgroundColor = ConsoleColor.Red
-                Else
-                    Console.BackgroundColor = ConsoleColor.Black
-                End If
-                Console.Write(" ")
-            Next
-        Next
+        '        If dot.Wall Then
+        '            Console.BackgroundColor = ConsoleColor.White
+        '        ElseIf dot.PathUsed Or dot.EndPoint Then
+        '            Console.BackgroundColor = ConsoleColor.Red
+        '        Else
+        '            Console.BackgroundColor = ConsoleColor.Black
+        '        End If
+        '        Console.Write(" ")
+        '    Next
+        'Next
 
 
     End Sub
