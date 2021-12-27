637b7145883eb330cf00114830d589e7b9797ee5
MkbMain <michael.bourke@live.co.uk>
2021-12-27 21:39:05 +0000

    Rename Clearer

--- PicSolver/GlobalSuppressions.vb
+++ PicSolver/GlobalSuppressions.vb
@@ -5,5 +5,5 @@
 
 Imports System.Diagnostics.CodeAnalysis
 
-<Assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification:="<Pending>", Scope:="member", Target:="~M:PicSolver.Map.LoadFromIamge(System.String)")>
-<Assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification:="<Pending>", Scope:="member", Target:="~M:PicSolver.Map.SaveSoultion(System.String)")>
+<Assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification:="<Pending>", Scope:="member", Target:="~M:MazePicSolver.Map.LoadFromIamge(System.String)")>
+<Assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification:="<Pending>", Scope:="member", Target:="~M:MazePicSolver.Map.SaveSoultion(System.String)")>
